cbe9cc11691074669bbe25ff8c1f4153c80e897a
Emcho0 <emir.kusundzija@gmail.com>
2025-04-05 21:38:29 +0200

    Prikazivanje teksta sa stilom i dodavanje komponente za ploču i ćeliju.

---
+++ src/components/Board.tsx
@@ -0,0 +1,18 @@
+export default function Board() {
+  return (
+    <view>
+      <text
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          display: "flex",
+          marginTop: "30px",
+          color: "#C6DFEC",
+          fontSize: "40px",
+        }}
+      >
+        Kako je narode
+      </text>
+    </view>
+  );
+}
